7a949ca1df23bd91aeb25cb709d92ad8a9c0cda6
Rogerio Von Czekus Drubi <rogeriodrubi@outlook.com>
2022-11-04 16:21:36 -0300

    Criação da pagina inicial e pagina de atribuições

--- lib/atribuicoes.dart
+++ lib/atribuicoes.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:url_launcher/url_launcher_string.dart';
+
 
 Color verde_escuro = const Color.fromARGB(255, 57, 120, 83);
 Color verde_escura = const Color.fromARGB(255, 34, 72, 52);
@@ -11,6 +15,16 @@ class atribuicoes extends StatefulWidget {
 }
 
 class _atribuicoesState extends State<atribuicoes> {
+  void launchExternalWebsite(String url) async {
+
+    var urlUri = Uri.parse(url);
+
+    if (await canLaunchUrl(urlUri)) {
+      await launchUrl(urlUri, mode: LaunchMode.externalApplication);
+    } else {
+      throw 'could not lanch $urlUri';
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,7 +46,7 @@ class _atribuicoesState extends State<atribuicoes> {
             ),
 
             Text(
-                '\nTextos de atribuições',
+                '\nAqui encontram-se as referencias para as informações e imagens contidas no app.',
                 textAlign: TextAlign.left,
                 style:
                 TextStyle(
@@ -40,6 +54,330 @@ class _atribuicoesState extends State<atribuicoes> {
                   fontWeight: FontWeight.bold,
                 )
             ),
+
+            TextButton.icon(
+              icon: Container(
+              ),
+              label: Text('https://brasilescola.uol.com.br/brasil/caatinga.htm',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500, color: Colors.blue[400]),
+              ),
+              onPressed: () => setState(() {
+                launchExternalWebsite("https://brasilescola.uol.com.br/brasil/caatinga.htm");
+              }),
+            ),
+
+            TextButton.icon(
+              icon: Container(
+              ),
+              label: Text('https://doi.org/10.1590%2F1809-4392201400423',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500, color: Colors.blue[400]),
+              ),
+              onPressed: () => setState(() {
+                launchExternalWebsite("https://doi.org/10.1590%2F1809-4392201400423");
+              }),
+            ),
+
+            TextButton.icon(
+              icon: Container(
+              ),
+              label: Text('https://commons.wikimedia.org/w/index.php?curid=5753884',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500, color: Colors.blue[400]),
+              ),
+              onPressed: () => setState(() {
+                launchExternalWebsite("https://commons.wikimedia.org/w/index.php?curid=5753884");
+              }),
+            ),
+
+            TextButton.icon(
+              icon: Container(
+              ),
+              label: Text('https://commons.wikimedia.org/w/index.php?curid=42981732',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500, color: Colors.blue[400]),
+              ),
+              onPressed: () => setState(() {
+                launchExternalWebsite("https://commons.wikimedia.org/w/index.php?curid=42981732");
+              }),
+            ),
+
+            TextButton.icon(
+              icon: Container(
+              ),
+              label: Text('https://www.mae-da-lua.org/port/contact.html#direitos',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500, color: Colors.blue[400]),
+              ),
+              onPressed: () => setState(() {
+                launchExternalWebsite("https://www.mae-da-lua.org/port/contact.html#direitos");
+              }),
+            ),
+
+            TextButton.icon(
+              icon: Container(
+              ),
+              label: Text('https://commons.wikimedia.org/w/index.php?curid=34452204',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500, color: Colors.blue[400]),
+              ),
+              onPressed: () => setState(() {
+                launchExternalWebsite("https://commons.wikimedia.org/w/index.php?curid=34452204");
+              }),
+            ),
+
+            TextButton.icon(
+              icon: Container(
+              ),
+              label: Text('https://www.flickr.com/photos/47108884@N07/4324561945',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500, color: Colors.blue[400]),
+              ),
+              onPressed: () => setState(() {
+                launchExternalWebsite("https://www.flickr.com/photos/47108884@N07/4324561945");
+              }),
+            ),
+
+            TextButton.icon(
+              icon: Container(
+              ),
+              label: Text('https://commons.wikimedia.org/w/index.php?curid=27464987',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500, color: Colors.blue[400]),
+              ),
+              onPressed: () => setState(() {
+                launchExternalWebsite("https://commons.wikimedia.org/w/index.php?curid=27464987");
+              }),
+            ),
+
+            TextButton.icon(
+              icon: Container(
+              ),
+              label: Text('https://commons.wikimedia.org/w/index.php?curid=27464986',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500, color: Colors.blue[400]),
+              ),
+              onPressed: () => setState(() {
+                launchExternalWebsite("https://commons.wikimedia.org/w/index.php?curid=27464986");
+              }),
+            ),
+
+            TextButton.icon(
+              icon: Container(
+              ),
+              label: Text('https://www.flickr.com/photos/35478170@N08/14266086433',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500, color: Colors.blue[400]),
+              ),
+              onPressed: () => setState(() {
+                launchExternalWebsite("https://www.flickr.com/photos/35478170@N08/14266086433");
+              }),
+            ),
+
+            TextButton.icon(
+              icon: Container(
+              ),
+              label: Text('https://commons.wikimedia.org/w/index.php?curid=2840223',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500, color: Colors.blue[400]),
+              ),
+              onPressed: () => setState(() {
+                launchExternalWebsite("https://commons.wikimedia.org/w/index.php?curid=2840223");
+              }),
+            ),
+
+            TextButton.icon(
+              icon: Container(
+              ),
+              label: Text('https://projetocaatinga.ufersa.edu.br/informacoes-gerais-craibeira/',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500, color: Colors.blue[400]),
+              ),
+              onPressed: () => setState(() {
+                launchExternalWebsite("https://projetocaatinga.ufersa.edu.br/informacoes-gerais-craibeira/");
+              }),
+            ),
+
+            TextButton.icon(
+              icon: Container(
+              ),
+              label: Text('https://commons.wikimedia.org/w/index.php?curid=64794136',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500, color: Colors.blue[400]),
+              ),
+              onPressed: () => setState(() {
+                launchExternalWebsite("https://commons.wikimedia.org/w/index.php?curid=64794136");
+              }),
+            ),
+
+            TextButton.icon(
+              icon: Container(
+              ),
+              label: Text('https://commons.wikimedia.org/w/index.php?curid=13340958',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500, color: Colors.blue[400]),
+              ),
+              onPressed: () => setState(() {
+                launchExternalWebsite("https://commons.wikimedia.org/w/index.php?curid=13340958");
+              }),
+            ),
+
+            TextButton.icon(
+              icon: Container(
+              ),
+              label: Text('https://www.flickr.com/photos/mauroguanandi/2865423420/',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500, color: Colors.blue[400]),
+              ),
+              onPressed: () => setState(() {
+                launchExternalWebsite("https://www.flickr.com/photos/mauroguanandi/2865423420/");
+              }),
+            ),
+
+            TextButton.icon(
+              icon: Container(
+              ),
+              label: Text('https://commons.wikimedia.org/w/index.php?curid=16892236',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500, color: Colors.blue[400]),
+              ),
+              onPressed: () => setState(() {
+                launchExternalWebsite("https://commons.wikimedia.org/w/index.php?curid=16892236");
+              }),
+            ),
+
+            TextButton.icon(
+              icon: Container(
+              ),
+              label: Text('https://commons.wikimedia.org/w/index.php?curid=87697273',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500, color: Colors.blue[400]),
+              ),
+              onPressed: () => setState(() {
+                launchExternalWebsite("https://commons.wikimedia.org/w/index.php?curid=87697273");
+              }),
+            ),
+
+            TextButton.icon(
+              icon: Container(
+              ),
+              label: Text('https://commons.wikimedia.org/w/index.php?curid=87697274',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500, color: Colors.blue[400]),
+              ),
+              onPressed: () => setState(() {
+                launchExternalWebsite("https://commons.wikimedia.org/w/index.php?curid=87697274");
+              }),
+            ),
+
+
+            TextButton.icon(
+              icon: Container(
+              ),
+              label: Text('https://commons.wikimedia.org/w/index.php?curid=12465312',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500, color: Colors.blue[400]),
+              ),
+              onPressed: () => setState(() {
+                launchExternalWebsite("https://commons.wikimedia.org/w/index.php?curid=12465312");
+              }),
+            ),
+
+            TextButton.icon(
+              icon: Container(
+              ),
+              label: Text('https://commons.wikimedia.org/w/index.php?curid=85074358',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500, color: Colors.blue[400]),
+              ),
+              onPressed: () => setState(() {
+                launchExternalWebsite("https://commons.wikimedia.org/w/index.php?curid=85074358");
+              }),
+            ),
+
+            TextButton.icon(
+              icon: Container(
+              ),
+              label: Text('https://appverde.wordpress.com/2015/09/30/embiratanha-pseudobombax-marginatum-2/',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500, color: Colors.blue[400]),
+              ),
+              onPressed: () => setState(() {
+                launchExternalWebsite("https://appverde.wordpress.com/2015/09/30/embiratanha-pseudobombax-marginatum-2/");
+              }),
+            ),
+
+            TextButton.icon(
+              icon: Container(
+              ),
+              label: Text('https://commons.wikimedia.org/w/index.php?curid=10948412',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500, color: Colors.blue[400]),
+              ),
+              onPressed: () => setState(() {
+                launchExternalWebsite("https://commons.wikimedia.org/w/index.php?curid=10948412");
+              }),
+            ),
+
+            TextButton.icon(
+              icon: Container(
+              ),
+              label: Text('https://commons.wikimedia.org/w/index.php?curid=19903197',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500, color: Colors.blue[400]),
+              ),
+              onPressed: () => setState(() {
+                launchExternalWebsite("https://commons.wikimedia.org/w/index.php?curid=19903197");
+              }),
+            ),
+
+            TextButton.icon(
+              icon: Container(
+              ),
+              label: Text('https://www.flickr.com/photos/tgerus/4092735474',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500, color: Colors.blue[400]),
+              ),
+              onPressed: () => setState(() {
+                launchExternalWebsite("https://www.flickr.com/photos/tgerus/4092735474");
+              }),
+            ),
+
+
+            TextButton.icon(
+              icon: Container(
+              ),
+              label: Text('https://commons.wikimedia.org/w/index.php?curid=78724875',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500, color: Colors.blue[400]),
+              ),
+              onPressed: () => setState(() {
+                launchExternalWebsite("https://commons.wikimedia.org/w/index.php?curid=78724875");
+              }),
+            ),
+
+            TextButton.icon(
+              icon: Container(
+              ),
+              label: Text('https://commons.wikimedia.org/w/index.php?curid=8380040',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500, color: Colors.blue[400]),
+              ),
+              onPressed: () => setState(() {
+                launchExternalWebsite("https://commons.wikimedia.org/w/index.php?curid=8380040");
+              }),
+            ),
+
+            TextButton.icon(
+              icon: Container(
+              ),
+              label: Text('https://commons.wikimedia.org/w/index.php?curid=4574717',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500, color: Colors.blue[400]),
+              ),
+              onPressed: () => setState(() {
+                launchExternalWebsite("https://commons.wikimedia.org/w/index.php?curid=4574717");
+              }),
+            ),
+
+            TextButton.icon(
+              icon: Container(
+              ),
+              label: Text('https://commons.wikimedia.org/w/index.php?curid=6805027',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500, color: Colors.blue[400]),
+              ),
+              onPressed: () => setState(() {
+                launchExternalWebsite("https://commons.wikimedia.org/w/index.php?curid=6805027");
+              }),
+            ),
+
+            TextButton.icon(
+              icon: Container(
+              ),
+              label: Text('https://commons.wikimedia.org/w/index.php?curid=54048444',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500, color: Colors.blue[400]),
+              ),
+              onPressed: () => setState(() {
+                launchExternalWebsite("https://commons.wikimedia.org/w/index.php?curid=54048444");
+              }),
+            ),
+
+
+
           ],
         ),),
 
--- lib/home.dart
+++ lib/home.dart
@@ -30,13 +30,91 @@ class _HomeState extends State<Home> {
 
       appBar: 
         AppBar(
-          title: Text("Caatinga Lens"),
+          title: Text("Menu"),
           backgroundColor: verde_escuro,
         ),
-      body: const Center(
-        child:
-          Text('My Page!'),
-        ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
+        child: Column(
+          children: [
+            Text(
+                'Caatinga Lens\n',
+                textAlign: TextAlign.center,
+                style:
+                TextStyle(
+                  // color: Colors.amber,
+                  color: verde_escuro,
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                )
+            ),
+
+            Text(
+                '    O app Caatinga Lens tem como objetivo facilitar o reconhecimento de espécies de plantas nativas'
+                ' da Caatinga.'
+                '\n    No canto superior esquerdo o usuário pode acessar o menu contendo:'
+                '\n    A camera para tirar uma foto da folha da planta ou escolher da galeria'
+                ' para que o app possa fazer o reconhecimento, '
+                '\n    Também há todo o catálogo de plantas cadastradas no aplicativo, com informações e curiosidades sobre'
+                ' a planta e algumas imagens para comparação'
+                '\n    E por fim uma pagina de atribuições onde se encontram todas as referencias bibliográficas '
+                'bem como os autores do app.',
+
+                textAlign: TextAlign.left,
+                style:
+                TextStyle(
+                  fontSize: 21,
+                  fontWeight: FontWeight.bold,
+                )
+            ),
+
+            SizedBox(
+              height: 15,
+            ),
+
+            Text(
+                'Bioma Caatinga\n',
+                textAlign: TextAlign.center,
+                style:
+                TextStyle(
+                  // color: Colors.amber,
+                  color: verde_escuro,
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                )
+            ),
+
+
+            Text(
+                '    Um bioma exclusivamente brasileiro compreendendo cerca de 11% '
+                'do território nacional e 70% do Nordeste do país, a Caatinga'
+                ' é rica na diversificação de fauna e flora, que se adapta às'
+                ' variações de clima, como aos longos períodos de estiagem.',
+
+                textAlign: TextAlign.left,
+                style:
+                TextStyle(
+                  fontSize: 21,
+                  fontWeight: FontWeight.bold,
+                )
+            ),
+
+            SizedBox(
+              height: 15,
+            ),
+
+            Container(
+                child:
+                Image.asset('assets/images/caatinga.png')
+            ),
+
+            SizedBox(
+              height: 15,
+            ),
+
+
+          ],
+        ),),
       drawer: Drawer(
         child: ListView(
           padding: EdgeInsets.zero,
